682ea4bc8658d06fec321e5c69de9abf0c8e53ab
xerneas <reachavinashs1@gmail.com>
2025-10-17 12:21:23 -0700

    updated tmux with catpucc

--- zsh/arch.zsh
+++ zsh/arch.zsh
@@ -2,4 +2,7 @@ export TERM="xterm-kitty"
 alias open="xdg-open"
 alias pbcopy="xclip -selection clipboard"
 alias pbpaste="xclip -selection clipboard -o"
+alias tm="tmux"
+alias ta="tmux attach"
+alias tmks="tmux kill-server"
 
